fe09c3378e4f2e3439f78bb30a283c4064f60eb4
Murat Dogu <dogumurat@gmail.com>
2020-09-14 13:04:51 +0300

    Initial Commit

---
+++ JackCompiler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JackCompiler
+//
+//  Created by Murat Dogu on 14.09.2020.
+//  Copyright © 2020 Murat Dogu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
